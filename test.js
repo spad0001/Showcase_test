20232433bdb6c522b1e4db2e080512157dce77f3
Rienzi Spadin <spad0001@hz.nl>
2022-10-13 12:25:57 +0200

    Final Assessment

---
+++ test.js
@@ -0,0 +1,11 @@
+if (grade < 5.5) {
+    console.log("insufficient");
+} if else (grade >= 5.5 && grade < 7) {
+    console.log("sufficient");
+} if else (grade >= 7 && grade < 9) {
+    console.log("good");
+} if else (grade >=9 && grade >= 10) {
+    console.log("excellent");
+} else (grade > 10) {
+    console.log("invalid");
+}
